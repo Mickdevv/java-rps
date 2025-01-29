0fbdad0f9af042026e1312da787c912150aa14bc
Mickdevv <michael@devereux.fm>
2025-01-29 18:02:49 +0100

    Initial commit

---
+++ src/main/java/com/example/java_rps/JavaRpsApplication.java
@@ -0,0 +1,14 @@
+package com.example.java_rps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRpsApplication.class, args);
+		System.out.println("Running");
+	}
+
+}
---
+++ src/test/java/com/example/java_rps/JavaRpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_rps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
