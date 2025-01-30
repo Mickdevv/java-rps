484318981efeb30474ec5d8775c5400859453ca1
Mark AA. <mark.albanese.alessandrini@gmail.com>
2025-01-30 11:03:02 +0100

    feat: add

--- src/main/java/com/example/java_rps/JavaRpsApplication.java
+++ src/main/java/com/example/java_rps/JavaRpsApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaRpsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JavaRpsApplication.class, args);
 		System.out.println("[SpringBoot] - Running...");
 	}
-
 }
